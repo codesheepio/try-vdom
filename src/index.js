7ed9aa073e20ac975fd4d838b5504d5a5fc229dc
Arnupharp Viratanapanu <Arnupharp Viratanapanu>
2016-09-30 17:25:00 +0700

    Add display list example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from './components/Button'
+import TodoList from './components/TodoList'
 
+const todos = ['aaa', 'bbb', 'ccc']
 ReactDOM.render(
-  <Button color={'red'} />,
+  <TodoList todos={todos} />,
   document.getElementById('react-root')
 )
