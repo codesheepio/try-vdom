4e820a03ab68c17621693d3121995baafae1f021
Arnupharp Viratanapanu <Arnupharp Viratanapanu>
2016-09-30 17:01:46 +0700

    Add example Button code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from './components/Button'
+
+ReactDOM.render(
+  <Button color={'red'} />,
+  document.getElementById('react-root')
+)
